00f49295665705fdcc22ebb46e66f749fb5e77c2
Andrew Kump <kumpandrew@gmail.com>
2021-03-28 22:47:08 -0400

    logic for enabled and error handling

--- src/js/script.js
+++ src/js/script.js
@@ -10,27 +10,43 @@ let count = 0;
 
 chrome.storage.sync.set({
   requestCount: count
-}, function () {});
+}, () => { });
 
-const startForceAvailability = async function () {
-  const response = await fetch('https://presence.teams.microsoft.com/v1/me/forceavailability/', {
-    'headers': {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${getAuthToken()}`
-    },
-    'body': '{"availability":"Available"}',
-    'method': 'PUT'
+const runForceAvailability = async function () {
+  chrome.storage.sync.get(['isEnabled'], async function (storage) {
+    const {
+      isEnabled
+    } = storage;
+
+    // undefined because this would only be set if the user opens the settings view and I'm not sure how to default this otherwise.
+    if (isEnabled || isEnabled === undefined) {
+      try {
+        const response = await fetch('https://presence.teams.microsoft.com/v1/me/forceavailability/', {
+          'headers': {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${getAuthToken()}`
+          },
+          'body': '{"availability":"Available"}',
+          'method': 'PUT'
+        });
+
+        if (response.ok) {
+          count += 1;
+
+          chrome.storage.sync.set({
+            requestCount: count
+          }, () => { });
+        }
+        console.log('MS Teams Always Available:');
+        console.log(response);
+      } catch {
+        console.log('MS Teams Always Available: HTTP req failed to /forceavailability');
+      }
+    } else {
+      console.log('MS Teams Always Available: currently disabled');
+    }
   });
-  console.group('MS Teams: Always Available');
-  console.log(response);
-  console.groupEnd();
-
-  count += 1;
-
-  chrome.storage.sync.set({
-    requestCount: count
-  }, function () {});
 };
 
-setInterval(startForceAvailability, 30 * 1000);
-startForceAvailability();
+setInterval(runForceAvailability, 30 * 1000);
+runForceAvailability();
--- src/js/settings.js
+++ src/js/settings.js
@@ -14,3 +14,41 @@ const updateRequestCount = function () {
 
 setInterval(updateRequestCount, 30 * 1000);
 updateRequestCount();
+
+let queued = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const checkbox = document.querySelector('input[type="checkbox"]');
+
+    checkbox.addEventListener('change', () => {
+        const savedTextElement = document.getElementById('saved-text');
+        if (savedTextElement.innerText === '') {
+            savedTextElement.innerText = 'Saved!';
+        }
+
+        if (!queued) {
+            setTimeout(() => {
+                queued = false;
+                savedTextElement.innerText = '';
+            }, 5000);
+        }
+
+        queued = true;
+
+        if (checkbox.checked) {
+            chrome.storage.sync.set({
+                isEnabled: true
+            }, function () { });
+        } else {
+            chrome.storage.sync.set({
+                isEnabled: false
+            }, function () { });
+        }
+    });
+    chrome.storage.sync.get(['isEnabled'], async function (storage) {
+        const {
+            isEnabled
+        } = storage;
+        checkbox.checked = isEnabled;
+    });
+});
