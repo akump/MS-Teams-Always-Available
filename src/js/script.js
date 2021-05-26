f0b3abef721d4e7e067e7c56ceb2ba3d9a456a0e
Andrew <kumpaw@mail.uc.edu>
2021-05-25 21:20:17 -0400

    choose status type feature

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@ let count = 0;
 
 chrome.storage.sync.set({
   requestCount: count
-}, () => { });
+}, () => {});
 
 const runForceAvailability = async function () {
   chrome.storage.sync.get(['isEnabled'], async storage => {
@@ -19,32 +19,56 @@ const runForceAvailability = async function () {
     } = storage;
 
     // undefined because this would only be set if the user opens the settings view and I'm not sure how to default this otherwise.
-    if (isEnabled || isEnabled === undefined) {
-      try {
-        const response = await fetch('https://presence.teams.microsoft.com/v1/me/forceavailability/', {
-          'headers': {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${getAuthToken()}`
-          },
-          'body': '{"availability":"Available"}',
-          'method': 'PUT'
-        });
-
-        if (response.ok) {
-          count += 1;
-
-          chrome.storage.sync.set({
-            requestCount: count
-          }, () => { });
-        }
-        console.log('MS Teams Always Available:');
-        console.log(response);
-      } catch {
-        console.log('MS Teams Always Available: HTTP req failed to /forceavailability');
+    chrome.storage.sync.get(['requestCount'], function (storage) {
+      const {
+        requestCount
+      } = storage;
+      const countElement = document.getElementById('count');
+      if (requestCount) {
+        countElement.innerHTML = requestCount;
+      } else {
+        countElement.innerHTML = 0;
       }
-    } else {
-      console.log('MS Teams Always Available: currently disabled');
-    }
+    });
+    chrome.storage.sync.get(['statusType'], async function (storage) {
+      const {
+        statusType
+      } = storage;
+
+      if (!statusType) {
+        chrome.storage.sync.set({
+          statusType: 'Available'
+        }, () => {});
+        statusType === 'Available';
+      }
+
+      if (isEnabled || isEnabled === undefined) {
+        try {
+          const response = await fetch('https://presence.teams.microsoft.com/v1/me/forceavailability/', {
+            'headers': {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${getAuthToken()}`
+            },
+            'body': `{"availability":"${statusType}"}`,
+            'method': 'PUT'
+          });
+
+          if (response.ok) {
+            count += 1;
+
+            chrome.storage.sync.set({
+              requestCount: count
+            }, () => {});
+          }
+          console.log('MS Teams Always Available:');
+          console.log(response);
+        } catch {
+          console.log('MS Teams Always Available: HTTP req failed to /forceavailability');
+        }
+      } else {
+        console.log('MS Teams Always Available: currently disabled');
+      }
+    });
   });
 };
 
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,3 +1,18 @@
+const idToStatus = {
+    'StatusCheck0': 'Available',
+    'StatusCheck1': 'Busy'
+};
+
+const selectOnlyThis = function (e) {
+    for (let i = 0; i <= 1; i++) {
+        document.getElementById(`StatusCheck${i}`).checked = false;
+    }
+    document.getElementById(e.currentTarget.id).checked = true;
+    chrome.storage.sync.set({
+        statusType: idToStatus[e.currentTarget.id]
+    }, () => {});
+};
+
 const updateRequestCount = function () {
     chrome.storage.sync.get(['requestCount'], function (storage) {
         const {
@@ -12,7 +27,7 @@ const updateRequestCount = function () {
     });
 };
 
-setInterval(updateRequestCount, 30 * 1000);
+setInterval(updateRequestCount, 15 * 1000);
 updateRequestCount();
 
 let queued = false;
@@ -45,6 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }, () => {});
         }
     });
+
     chrome.storage.sync.get(['isEnabled'], async storage => {
         const {
             isEnabled
@@ -59,6 +75,36 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             checkbox.checked = isEnabled;
         }
+    });
 
+    const availableStatusElement = document.getElementById("StatusCheck0");
+    const busyStatusElement = document.getElementById("StatusCheck1");
+
+    availableStatusElement.addEventListener("click", selectOnlyThis);
+    busyStatusElement.addEventListener("click", selectOnlyThis);
+
+    chrome.storage.sync.get(['statusType'], async storage => {
+        const {
+            statusType
+        } = storage;
+
+        // handle use case where extension was just installed
+        if (statusType === undefined) {
+            chrome.storage.sync.set({
+                statusType: 'Available'
+            }, () => {});
+            availableStatusElement.checked = true;
+        } else {
+            if (statusType === 'Available') {
+                availableStatusElement.checked = true;
+                busyStatusElement.checked = false;
+            } else if (statusType === 'Busy') {
+                busyStatusElement.checked = true;
+                availableStatusElement.checked = false;
+            } else {
+                availableStatusElement.checked = true;
+                busyStatusElement.checked = false;
+            }
+        }
     });
 });
