acc5fce4c89a2fb13042edeec2b0049a645dbb70
Andrew Kump <kumpandrew@gmail.com>
2022-02-01 18:54:41 -0500

    semicolon

--- src/js/script.js
+++ src/js/script.js
@@ -57,7 +57,7 @@ const requestForceAvailability = function () {
             statusType === 'Available';
         }
         if (!paid) {
-            console.log('User does not have an active subscription')
+            console.log('User does not have an access');
             return;
         }
 
